02ec11f6893a8fb4ce234a514da4a348adecaa75
Skanayama <74477869+Skanayama@users.noreply.github.com>
2021-02-07 15:55:35 +0900

    ユーザー管理機能_part3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :encrypted_password, :nickname, :family_name, :first_name, :furi_family_name, :furi_first_name, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :furi_family_name, :furi_first_name, :birthday])
   end
 end
